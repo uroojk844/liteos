e349af94e1dcc55e66d850c42970ba1e99111e37
Urooj Khan <uroojk844@gmail.com>
2019-09-23 23:41:35 +0530

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,48 @@
+<div class="w3-bar">
+<div class="w3-bar-item w3-right w3-text-white w3-hover-red"
+     onclick="minimise('setting')"><i class="fas fa-times"></i></div>
+</div>
+
+<div id="name"><i class="fas fa-cog"></i> Setting</div>
+
+<div id="setting_container" class="w3-row">
+
+    <div class="w3-row w3-col l4 m4 s12 w3-margin-bottom">
+        <div class="w3-col s2 m4">
+            <i class="far fa-image"></i>
+        </div>
+        <div class="w3-col s10 m8 w3-left">
+            Wallpaper
+        </div>
+    </div>
+
+    <div class="w3-row w3-col l4 m4 s12 w3-margin-bottom">
+        <div class="w3-col s2 m4">
+            <i class="fas fa-download"></i>
+        </div>
+        <div class="w3-col s10 m8 w3-left">
+            Add app
+        </div>
+    </div>
+
+    <div class="w3-row w3-col l4 m4 s12 w3-margin-bottom">
+        <div class="w3-col s2 m4">
+            <i class="far fa-image"></i>
+        </div>
+        <div class="w3-col s10 m8 w3-left">
+            Wallpaper
+        </div>
+    </div>
+
+    <div class="w3-row w3-col l4 m4 s12 w3-margin-bottom">
+        <div class="w3-col s2 m4">
+            <i class="far fa-user"></i>
+        </div>
+        <div class="w3-col s10 m8 w3-left">
+            About
+        </div>
+    </div>
+
+</div>
+
+<!--4285F4, #FBBC05, #34A853, #EA4335-->
